b755380a6550f8855bfd18eeec003a28f630a84e
Eric Grant <senatorericgrant@gmail.com>
2022-05-19 23:41:36 +0700

    feat: create the data model

---
+++ MyStoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  MyStoreSearch
+//
+//  Created by Eric Grant on 19.05.2022.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
