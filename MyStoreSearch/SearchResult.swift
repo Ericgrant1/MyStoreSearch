46d8495ad1ad7c6e903d3a87d4fe8d066d9cc415
Eric Grant <senatorericgrant@gmail.com>
2022-05-26 23:54:09 +0700

    refactor: modify data model to load the value fo the above key

--- MyStoreSearch/SearchResult.swift
+++ MyStoreSearch/SearchResult.swift
@@ -15,13 +15,20 @@ class ResultArray: Codable {
 class SearchResult: Codable, CustomStringConvertible {
     var artistName: String? = ""
     var trackName: String? = ""
+    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var currency = ""
+    var artworkUrl60 = ""
+    var artworkUrl100 = ""
+    var trackViewUrl: String? = ""
+    var primaryGenreName = ""
     
     var name: String {
         return trackName ?? ""
     }
     
     var description: String {
-        return "\nResult - Name: \(name), Artist Name: \(artistName ?? "None")"
+        return "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
     }
 }
 
