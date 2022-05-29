06aa060e7ddc1055e1321d95fad388971be33998
Eric Grant <senatorericgrant@gmail.com>
2022-05-29 23:31:38 +0700

    feat: implement the activity indicator cell

--- MyStoreSearch/SearchViewController.swift
+++ MyStoreSearch/SearchViewController.swift
@@ -14,11 +14,13 @@ class SearchViewController: UIViewController {
     
     var searchResults = [SearchResult]()
     var hasSearched = false
+    var isLoading = false
     
     struct TableView {
         struct CellIdentifiers {
             static let searchResultCell = "SearchResultCell"
             static let nothingFoundCell = "NothingFoundCell"
+            static let loadingCell = "LoadingCell"
         }
     }
 
@@ -30,6 +32,12 @@ class SearchViewController: UIViewController {
         cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
         searchBar.becomeFirstResponder()
+        cellNib = UINib(
+            nibName: TableView.CellIdentifiers.loadingCell,
+            bundle: nil)
+        tableView.register(
+            cellNib,
+            forCellReuseIdentifier: TableView.CellIdentifiers.loadingCell)
     }
 
     // MARK: - Helper Methods
@@ -85,6 +93,9 @@ extension SearchViewController: UISearchBarDelegate {
         if !searchBar.text!.isEmpty {
             searchBar.resignFirstResponder()
             
+            isLoading = true
+            tableView.reloadData()
+            
             hasSearched = true
             searchResults = []
             
@@ -95,6 +106,8 @@ extension SearchViewController: UISearchBarDelegate {
                 searchResults = parse(data: data)
                 searchResults.sort(by: <)
               }
+            
+            isLoading = false
             tableView.reloadData()
         }
     }
@@ -110,7 +123,9 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
     ) -> Int {
-        if !hasSearched {
+        if isLoading {
+            return 1
+        } else if !hasSearched {
             return 0
         } else if searchResults.count == 0 {
             return 1
@@ -123,7 +138,15 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         _ tableView: UITableView,
         cellForRowAt indexPath: IndexPath
     ) -> UITableViewCell {
-        if searchResults.count == 0 {
+        if isLoading {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TableView.CellIdentifiers.loadingCell,
+                for: indexPath)
+            
+            let spinner = cell.viewWithTag(100) as! UIActivityIndicatorView
+            spinner.startAnimating()
+            return cell
+        } else if searchResults.count == 0 {
             return tableView.dequeueReusableCell(
                 withIdentifier: TableView.CellIdentifiers.nothingFoundCell,
                 for: indexPath)
@@ -157,7 +180,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         _ tableView: UITableView,
         willSelectRowAt indexPath: IndexPath
     ) -> IndexPath? {
-        if searchResults.count == 0 {
+        if searchResults.count == 0 || isLoading {
             return nil
         } else {
             return indexPath
