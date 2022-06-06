77bda0f441fcc9875765acbeb93a9fddd0d92567
Eric Grant <senatorericgrant@gmail.com>
2022-06-06 23:06:59 +0700

    feat: dismiss the pop-up by tapping anywhere outsude it

--- MyStoreSearch/DetailViewController.swift
+++ MyStoreSearch/DetailViewController.swift
@@ -20,21 +20,27 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         popupView.layer.cornerRadius = 10
+        
+        let gesturerecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(close))
+        gesturerecognizer.cancelsTouchesInView = false
+        gesturerecognizer.delegate = self
+        view.addGestureRecognizer(gesturerecognizer)
     }
     
     // MARK: - Actions
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - Gesture Recognizer Delegate
+extension DetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldReceive touch: UITouch
+    ) -> Bool {
+        return (touch.view === self.view)
     }
-    */
-
 }
