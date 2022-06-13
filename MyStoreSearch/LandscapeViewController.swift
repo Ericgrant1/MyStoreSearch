782d0e8ead980caa73af3a0d952bff5cf069bda0
Eric Grant <senatorericgrant@gmail.com>
2022-06-13 19:36:40 +0700

    feat: set the scroll view content size

--- MyStoreSearch/LandscapeViewController.swift
+++ MyStoreSearch/LandscapeViewController.swift
@@ -25,6 +25,8 @@ class LandscapeViewController: UIViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = true
         
         view.backgroundColor = UIColor(patternImage: UIImage(named: "LandscapeBackground")!)
+        
+        scrollView.contentSize = CGSize(width: 1000, height: 1000)
     }
     
     override func viewWillLayoutSubviews() {
