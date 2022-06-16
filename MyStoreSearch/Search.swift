206add85e638d38725bca6b4fab06868e78f2afa
Eric Grant <senatorericgrant@gmail.com>
2022-06-16 22:52:42 +0700

    refactor: add the search logic back in

--- MyStoreSearch/Search.swift
+++ MyStoreSearch/Search.swift
@@ -12,10 +12,37 @@ class Search {
     var hasSearched = false
     var isLoading = false
     
-    private var datatask: URLSessionDataTask?
+    private var dataTask: URLSessionDataTask?
     
     func performSearch(for text: String, category: Int) {
-        print("Searching...")
+        if !text.isEmpty {
+            dataTask?.cancel()
+            
+            isLoading = true
+            hasSearched = true
+            searchResults = []
+            
+            let url = iTunesURL(searchText: text, category: category)
+            let session = URLSession.shared
+            dataTask = session.dataTask(with: url) { data, response, error in
+                // Was the search cancelled?
+                if let error = error as NSError?, error.code == -999 {
+                    return
+                }
+                if let httpResponse = response as? HTTPURLResponse,
+                   httpResponse.statusCode == 200, let data = data {
+                    self.searchResults = self.parse(data: data)
+                    self.searchResults.sort(by: <)
+                    print("Success!")
+                    self.isLoading = false
+                    return
+                }
+                print("Failure! \(response!)")
+                self.hasSearched = false
+                self.isLoading = false
+            }
+            dataTask?.resume()
+        }
     }
     
     // MARK: - Private Methods
--- MyStoreSearch/SearchViewController.swift
+++ MyStoreSearch/SearchViewController.swift
@@ -134,47 +134,6 @@ extension SearchViewController: UISearchBarDelegate {
             category: segmentedControl.selectedSegmentIndex)
         tableView.reloadData()
         searchBar.resignFirstResponder()
-        
-//        if !searchBar.text!.isEmpty {
-//            searchBar.resignFirstResponder()
-//
-//            dataTask?.cancel()
-//            isLoading = true
-//            tableView.reloadData()
-//
-//            hasSearched = true
-//            searchResults = []
-//
-//            let url = iTunesURL(searchText: searchBar.text!,
-//                                category: segmentedControl.selectedSegmentIndex)
-//            let session = URLSession.shared
-//            dataTask = session.dataTask(with: url) { data, response, error in
-//
-//                if let error = error as NSError?, error.code == -999 {
-//                    return // Search was cancelled
-//                } else if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
-//
-//                    if let data = data {
-//                        self.searchResults = self.parse(data: data)
-//                        self.searchResults.sort(by: <)
-//                        DispatchQueue.main.async {
-//                            self.isLoading = false
-//                            self.tableView.reloadData()
-//                        }
-//                        return
-//                    }
-//                } else {
-//                    print("Failure! \(response!)")
-//                }
-//                DispatchQueue.main.async {
-//                    self.hasSearched = false
-//                    self.isLoading = false
-//                    self.tableView.reloadData()
-//                    self.showNetworkError()
-//                }
-//            }
-//            dataTask?.resume()
-//        }
     }
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
